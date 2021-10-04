1ca7672720610660c53a39078a7815cb88f5fbac
jordanbradburne <85847344+jordanbradburne@users.noreply.github.com>
2021-10-03 17:42:01 -0700

    Deliverable 1

---
+++ MegaCarChallenge.RScript.R
@@ -0,0 +1,12 @@
+
+#load dplyr package
+library(dplyr)
+
+#read file
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+#perform summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
